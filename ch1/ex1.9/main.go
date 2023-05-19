d44306d4a44ab58d7b7558f8486b40b60f7c1e15
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-05-19 12:29:04 +0800

    exercise 1.9: fitst commit

---
+++ ch1/ex1.9/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	prefix := "http://"
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
+		resp, err := http.Get(url)
+		status := resp.Status
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		c, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%d", c)
+		fmt.Println(status)
+	}
+}
+
+//!-
